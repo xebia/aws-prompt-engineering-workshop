d19a02d49b4f47c2d93ad926c449dfc05d83bb46
Joris Conijn <joris@conijnonline.nl>
2025-10-15 17:32:31 +0200

    fix: utils import in notebooks

--- utils/run_managed_prompt.py
+++ utils/run_managed_prompt.py
@@ -1,3 +1,4 @@
+import json
 from typing import Dict
 
 import boto3
@@ -7,9 +8,7 @@ client = boto3.client("bedrock-runtime")
 
 def run_managed_prompt(prompt_arn: str, input_vars: Dict[str, str]) -> str:
     response = client.invoke_model(
-        modelId=prompt_arn,
-        contentType="text/plain",
-        accept="application/json",
+        modelId=prompt_arn, body=json.dumps({"promptVariables": input_vars})
     )
     result = response["body"].read().decode("utf-8")
     return result
